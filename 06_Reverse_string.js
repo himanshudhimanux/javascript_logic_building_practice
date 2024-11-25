b17329fb16855621207baed78c991754c0d5d23c
himanshudhimanux <himanshudhiman.hd34@gmail.com>
2024-11-25 12:20:38 +0530

    Reverse String solution with Array and For Loop

--- 06_Reverse_string.js
+++ 06_Reverse_string.js
@@ -3,6 +3,12 @@
 // ==== with For loop
 
 function reverseString(str){
+
+    if(typeof str !== 'string'){
+        throw new Error("Only Aplhabet are allwoed");
+        
+    }
+
     let result = "";
     for(let i = str.length - 1; i>=0; i--){
         result += str[i]
@@ -16,11 +22,17 @@ console.log('Reverse String with For loop =>', reverseString('Paras'));
 // ======== With Array
 
 function reverseStringArr(arrStr){
+
+    if(typeof str !== 'string'){
+        throw new Error("Only Aplhabet are allwoed");
+        
+    }
+
     const result = arrStr.split('').reverse().join('');
     return result
 }
 
-console.log('Reverse String Array =>', reverseStringArr('Himanshu'));
+console.log('Reverse String Array =>', reverseStringArr(45));
 
 
 
